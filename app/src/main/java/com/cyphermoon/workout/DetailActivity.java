3774d94abbfed1976e402aee8a65ff7af4832476
Cypher Moon <temperaturecaleb@gmail.com>
2022-07-14 11:19:15 +0100

    Coordinated the fragment

--- app/src/main/java/com/cyphermoon/workout/DetailActivity.java
+++ app/src/main/java/com/cyphermoon/workout/DetailActivity.java
@@ -2,9 +2,12 @@ package com.cyphermoon.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +15,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.workout_detail_fragment);
-        frag.setWorkoutId(2L);
+        Intent intent = getIntent();
+        long id = (long) intent.getExtras().get(EXTRA_WORKOUT_ID);
+        frag.setWorkoutId(id);
     }
 }
--- app/src/main/java/com/cyphermoon/workout/WorkoutListFragment.java
+++ app/src/main/java/com/cyphermoon/workout/WorkoutListFragment.java
@@ -1,7 +1,9 @@
 package com.cyphermoon.workout;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
@@ -9,8 +11,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutListFragment extends ListFragment {
+
+    static interface Listener{
+        void itemClicked(long id);
+    }
+
+    private Listener listener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,5 +46,12 @@ public class WorkoutListFragment extends ListFragment {
 
         setListAdapter(arrayAdapter);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if(listener != null) listener.itemClicked(position);
     }
 }
